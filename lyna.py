9add01ca664441dd51b4cfcb3069372449ef57d0
Peter Hicks <peter@hicks.im>
2019-09-18 14:17:41 +0100

    Make welcoming work correctly

--- lyna.py
+++ lyna.py
@@ -1,14 +1,18 @@
 import os
 import discord
+import logging
 
+logging.basicConfig(level=logging.INFO)
 client = discord.Client()
 
 token = os.environ.get('TOKEN')
-new_member_role = os.environ.get('NEW_MEMBER_ROLE')
-moderator_role = os.environ.get('MODERATOR_ROLE')
-welcome_channel = os.environ.get('WELCOME_CHANNEL')
 
-rules_channel = client.get_channel(os.environ.get('RULES_CHANNEL'))
+new_member_role = int(os.environ.get('NEW_MEMBER_ROLE'))
+moderator_role = int(os.environ.get('MODERATOR_ROLE'))
+
+welcome_channel = int(os.environ.get('WELCOME_CHANNEL'))
+rules_channel = client.get_channel(int(os.environ.get('RULES_CHANNEL')))
+
 welcome_message = """:sparkles: **Welcome to {0.guild.name}, {0.mention}!** :sparkles:
 
 **Please read through #{1} before continuing.** Once you're done, please let one of the <@{2}> know your gender in order to get access to the full server.
@@ -18,17 +22,28 @@ Your options are **boys**, **girls**, **nonbinary**, and **genderfluid**. You ca
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    activity = discord.Activity(name='for signs of trouble', type=discord.ActivityType.listening)
+    logging.info('We have logged in as {0.user}'.format(client))
+    activity = discord.Activity(name='over newcomers', type=discord.ActivityType.listening)
     await client.change_presence(activity=activity)
 
 @client.event
-async def on_member_update(before, after):
-    new_roles = (set(before.roles) - set(after.roles))
+async def on_member_update(prev_user, user):
+    removed_roles = (set(prev_user.roles) - set(user.roles))
 
-    for role in new_roles:
+    for role in removed_roles:
         if role.id == new_member_role:
-            welcome_channel = client.get_channel(welcome_channel)
-            await welcome_channel.send(welcome_message.format(after, rules_channel, moderator_role))
+            try:
+                await client.get_channel(welcome_channel).send(welcome_message.format(user, rules_channel, moderator_role))
+                logging.info('Welcomed {0.name} successfully.'.format(user))
+                return
+            except Forbidden:
+                logging.error('We don\'t have permission to send welcome messages.')
+                return
+            except HTTPException:
+                logging.error('Sending the message failed due to an HTTP error.')
+                return
+            except:
+                logging.error('Something unknown has gone wrong.')
+                return
 
 client.run(token)
